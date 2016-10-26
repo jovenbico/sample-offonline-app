5b0b7ac6878244238ee3b132e6fd35e8ce3d62ab
jovenbico <jfbico@xmnt.net>
2016-10-26 18:11:22 +0800

    add thread delay on test

--- sample-evetbus/src/main/java/com/bicjo/eventbus/EventHandler.java
+++ sample-evetbus/src/main/java/com/bicjo/eventbus/EventHandler.java
@@ -5,6 +5,12 @@ public class EventHandler implements IEvent {
 	@Override
 	public void fireEvent(EventObj e) {
 		System.out.println("fire event: " + e.getId());
+
+		try {
+			Thread.sleep(3 * 1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 }
